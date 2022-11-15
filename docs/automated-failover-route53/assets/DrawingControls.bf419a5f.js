f3f2c00650e2db868bd9a2a90b9200b0e18168d3
Sathyajith Bhat <sathya@sathyasays.com>
2022-11-15 19:04:40 +1100

    update slides for Route 53 talk

---
+++ docs/automated-failover-route53/assets/DrawingControls.bf419a5f.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.0aa613d9.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.0aa613d9.js";import"./index.b4328223.js";export{t as default};
